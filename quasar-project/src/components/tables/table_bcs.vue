8c37c38d277bbafe269b5d3da367735dc24220f5
Toan Quach <quachphuongtoanc2@gmail.com>
2022-07-04 15:27:26 +0700

    change color

--- quasar-project/src/components/tables/table_bcs.vue
+++ quasar-project/src/components/tables/table_bcs.vue
@@ -1,60 +1,6 @@
 <template>
   <div class="q-pa-md">
-    <!--     <div class="tbheader">
-      <div class="header row justify-center full-width text-bold header-bcs">
-        <div class="col-md-1">STT </div>
-        <div class="col-md-4">Chỉ tiêu</div>
-        <div class="col-md-1">ĐVT</div>
-        <div class="col-md-1">Kế hoạch</div>
-        <div class="col-md-2">thực hiện</div>
-        <div class="col-md-1">% sosacnh</div>
-        <div class="col-md-2">cảnh báo</div>
-      </div>
-      <div class="full-width" v-for="(topic, index) in tables" :key="topic.id">
-        <div class="full-width row">
-          <div class="col-md-1">{{ ++index }}</div>
-          <div class="col-md-4 topic-name">{{ topic.name }}</div>
-          <div class="col-md-1">{{topic.comment}}</div>
-          <div class="col-md-1"></div>
-          <div class="col-md-2"></div>
-          <div class="col-md-1"></div>
-          <div class="col-md-1"></div>
-          <hr class="col-md-12" />
-        </div>
-        <div
-          class="full-width row "
-          v-for="(target1, index1) in topic.targets"
-          :key="index1"
-        >
-          <div class="col-md-1 target-level"> {{++index-index+1}}.{{ ++index1 }}</div>
-          <div class="col-md-4">{{ target1.name }}</div>
-          <div class="col-md-1">{{target1.comment}}</div>
-          <div class="col-md-1"> {{target1.setindicators[0].plan}}</div>
-          <div class="col-md-2"> {{ target1.setindicators[0].total_plan }}  </div>
-          <div class="col-md-1">{{ tinhphantram(target1.setindicators[0].total_plan , target1.setindicators[0].plan) }}</div>
-          <div class="col-md-1" :class=" tinhphantram(target1.setindicators[0].total_plan, target1.setindicators[0].plan) < 100 ? 'bg-red' : 'bg-green' " >
-            </div>
-          <hr class="col-md-12" />
-          <br />
-          <div class="full-width target-level" v-if="target1.targets.length > 1">
-            <div
-              class="full-with row"
-              v-for="(target2, index2) in target1.targets"
-              :key="index2"
-            >
-              <div class="col-md-1 target-level">{{ ++index-index1-1 }}.{{ ++index1-index2-1 }}.{{ ++index2 }}</div>
-              <div class="col-md-4 textover">{{ target2.name }}</div>
-              <div class="col-md-1"></div>
-              <div class="col-md-1"></div>
-              <div class="col-md-1"></div>
-              <div class="col-md-1"></div>
-              <div class="col-md-1"></div>
-              <hr class="col-md-12" />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div> -->
+
     <!--  TẠO BẢNG BẰNG QTABLE -->
     <!-- <q-btn color="primary" icon="check" label="OK" @click="canhbaomucdo('95', '100', '90')" /> -->
     <q-markup-table wrap-cells bordered >
@@ -90,7 +36,7 @@
 
       <tbody>
         <template v-for="(topic, index) in tables" :key="topic.id">
-          <tr class="text-left bg-cyan-3">
+          <tr class="text-left bg-cyan-14">
             <td class="top-size">
               <span class="topic-id">{{ ++index }}</span>
             </td>
