52a6f22cc4b955fe46058471a953cc28838482c0
Toan Quach <quachphuongtoanc2@gmail.com>
2022-07-19 11:26:26 +0700

    chinh port

--- quasar-project/quasar.config.js
+++ quasar-project/quasar.config.js
@@ -78,7 +78,7 @@ module.exports = configure(function (ctx) {
       server: {
         type: "http",
       },
-      port: 8080,
+      port: 80,
       open: true, // opens browser window automatically
     },
 
